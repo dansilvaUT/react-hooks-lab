4a3cb1439ba80af050b8dcef0f7f0b5b19e6fa48
dansilvaUT <dan13silv@gmail.com>
2021-01-21 14:48:06 -0800

    finished app 2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Facts from './Components/Facts';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [facts, setFacts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => setFacts(res.data.results))
+      .catch(err => console.log(`Error: ${err.message}`));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {facts.map((element, index) => {
+        return (
+          <Facts name={element.name} key={index} />
+        )
+      })}
+
     </div>
   );
 }
---
+++ app-2/src/Components/Facts.js
@@ -0,0 +1,9 @@
+const Facts = props => {
+    return (
+        <section>
+            <p>{props.name}</p>
+        </section>
+    )
+}
+
+export default Facts;
